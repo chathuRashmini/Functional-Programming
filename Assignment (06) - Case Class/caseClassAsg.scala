9a97ca5590da36744657f43d289d13a61f41844c
chathuRashmini <64759099+chathuRashmini@users.noreply.github.com>
2020-08-06 17:36:21 +0530

    Add files via upload

---
+++ Assignment (06) - Case Class/caseClassAsg.scala
@@ -0,0 +1,34 @@
+object caseClassAsg extends App {
+	val p1 = Point(2,3)
+	val p2 = Point(1,2)
+
+	println("p1 = " + p1)
+	println("p2 = " + p2)
+
+	val p3: Point = p1.add(p2)
+	println("p1 + p2 = " + p3)
+
+	var p4: Point = p1.move(10,20)
+	println("When p1 is moved 10 points along x axis\n\tand 20 points along y aixs = " + p4)
+
+	/*println("Distance between p1 and p2 = " + p1.distance(p2))*/
+
+	println("When x and y cordinates of p1 is switched = " + p1.invert())
+}
+
+case class Point(a: Int, b: Int) {
+	def x: Int = a
+	def y: Int = b
+
+	def add(p:Point): Point = Point(this.x+p.x,this.y+p.y)
+
+	def move(dx:Int, dy:Int): Point = Point(this.x+dx, this.y+dy)
+
+	/*def distance(p:Point): Int = {
+		def dx: Int = this.x - dx;
+		def dy: Int = this.y - dy;
+		return dx*dx + dy*dy;
+	}*/
+
+	def invert(): Point = Point (this.y , this.x)
+}
